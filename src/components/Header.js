79706cd93682545f0bacd863c7434178414937b0
Ameya Raje <ameyacraje@gmail.com>
2020-11-22 18:22:22 -0500

    dependencies added

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Header = () => {
+    render(
+        <div>
+
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const Landing = () => {
+    render(
+        <div>
+            
+        </div>
+    );
+};
+
+export default Landing;
