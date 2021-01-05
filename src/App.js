8d7b64c0d39eda51cb5aa966d0a71c6f70938a3e
Ameya Raje <ameyacraje@gmail.com>
2021-01-04 21:52:16 -0500

    reorganization and minor updates to landing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Router, Switch, Route } from "react-router-dom";
+import Landing from './components/Landing';
+import { createBrowserHistory } from 'history';
+export const history = createBrowserHistory();
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={history}>
+      <Switch>
+        <Route exact component={Landing} path="/" />
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,10 +1,18 @@
-import { render } from '@testing-library/react';
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const Landing = () => {
-    render(
+    return (
         <div>
-            
+            <div className="jumbotron jumbotron-fluid">
+                <div className="container">
+                    <h1 className="display-4">Book Summarizer</h1>
+                    <p className="lead">A glorified notes app. </p>
+                </div>
+            </div>
+            <div className="container">
+                <p>I'm learning ReactJS and have been saving notes when I read books using MS Word. This is an application to learn ReactJS while making something I would value.</p>
+            </div>
         </div>
     );
 };
