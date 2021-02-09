0c65e14182c608381829abfcb68ec348a14d056c
Ameya Raje <ameyacraje@gmail.com>
2021-02-09 18:58:23 -0500

    init data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import configureStore from './store/configureStore';
+import { addNote } from './actions/notes';
+import { searchByAuthor } from './actions/filters';
+import filteredNotes from './selectors/filteredNotes';
+import { Provider } from 'react-redux';
 
+const store = configureStore();
+store.dispatch(addNote({ author: 'Jerry Groopman', title: 'How Doctors Think', text: 'blah' }));
+store.dispatch(addNote({ author: 'Jerry Tom', title: 'A Cartoon', text: 'hmm' }));
+store.dispatch(addNote({ author: 'Michelle Obama', title: 'Becoming', text: 'Interesting stuff' }));
+// store.dispatch(searchByAuthor('Jerry Tom'));
+const state = store.getState();
+const visibleNotes = filteredNotes(state.notes, state.filters);
+console.log("VN", visibleNotes);
+console.log(store.getState());
+
+const jsx = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 ReactDOM.render(
-  <App />,
+  jsx,
   document.getElementById('root')
 );
 
